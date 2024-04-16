4401d464a5a070b05ac80bc5598f974befdf6f5e
Timothe Dutel <tdutel@z2r9p6.42lyon.fr>
2024-04-16 14:39:03 +0200

    add color file

---
+++ incs/Color.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Color.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 14:38:42 by tdutel            #+#    #+#             */
+/*   Updated: 2024/04/16 14:38:44 by tdutel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLOR_HPP
+# define COLOR_HPP
+
+#define RESET "\033[0m"
+#define GREY "\x1b[30m"
+#define RED "\x1b[31m"
+#define GREEN "\x1b[32m"
+#define YELLOW "\x1b[33m"
+#define BLUE "\x1b[34m"
+#define PURPLE "\x1b[35m"
+#define CYAN "\x1b[36m"
+
+/*
+Name            FG  BG
+Black           30  40
+Red             31  41
+Green           32  42
+Yellow          33  43
+Blue            34  44
+Magenta         35  45
+Cyan            36  46
+White           37  47
+Bright Black    90  100
+Bright Red      91  101
+Bright Green    92  102
+Bright Yellow   93  103
+Bright Blue     94  104
+Bright Magenta  95  105
+Bright Cyan     96  106
+Bright White    97  107
+*/
+
+#endif
